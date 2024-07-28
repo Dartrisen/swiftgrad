2c331b2e14da00cb3495e47fa42e976610ad0bbc
dartrisen <calculating.scientist@gmail.com>
2024-07-28 16:56:32 +0200

    adding new functionality: operation labels, children nodes

--- value.swift
+++ value.swift
@@ -7,31 +7,56 @@ protocol ArithmeticOperations {
 
 final class Value: ArithmeticOperations, CustomStringConvertible {
     var data: Double
+    var _prev: Set<Value>
+    var _op: String
+    var label: String
 
-    init(_ data: Double) {
+    init(_ data: Double, _children: [Value] = [], _op: String = "", label: String = "") {
         self.data = data
+        self._prev = Set(_children)
+        self._op = _op
+        self.label = label
     }
 
     var description: String {
-        return "Value(data: \(data))"
+        return "Value(label: \(label), data: \(data))"
     }
 
     static func + (lhs: Value, rhs: Value) -> Value {
-        let out = Value(lhs.data + rhs.data)
+        let out = Value(lhs.data + rhs.data, _children: [lhs, rhs], _op: "+")
         return out
     }
 
     static func * (lhs: Value, rhs: Value) -> Value {
-        let out = Value(lhs.data * rhs.data)
+        let out = Value(lhs.data * rhs.data, _children: [lhs, rhs], _op: "*")
         return out
     }
 }
 
+extension Value: Hashable {
+    static func == (lhs: Value, rhs: Value) -> Bool {
+        return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+}
+
 // Define inputs
-let x1 = Value(2.0)
-let x2 = Value(0.0)
+let x1 = Value(2.0, label: "x1")
+let x2 = Value(0.0, label: "x2")
 
-let w1 = Value(-3.0)
-let w2 = Value(1.0)
+// Define weights
+let w1 = Value(-3.0, label: "w1")
+let w2 = Value(1.0, label: "w2")
 
-print(x1 * w1 + x2 * w2)
+// Define bias
+let b = Value(6.8813735870195432, label: "b")
+
+// Perform operations
+let x1w1 = x1 * w1
+x1w1.label = "x1*w1"
+
+// Print results
+print(x1w1)
