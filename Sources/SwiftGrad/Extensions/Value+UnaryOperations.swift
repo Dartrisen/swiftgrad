c6fd34813c413bf8eef61dd79e4ef34ff7662486
dartrisen <calculating.scientist@gmail.com>
2024-09-05 21:22:35 +0200

    🆕 log func has been added

--- Sources/SwiftGrad/Extensions/Value+UnaryOperations.swift
+++ Sources/SwiftGrad/Extensions/Value+UnaryOperations.swift
@@ -7,8 +7,21 @@ extension Value: UnaryOperations {
     /// - Returns: A new `Value` representing the result of `exp`.
     func exp() -> Value {
         let out = Value(Foundation.exp(self.data), _children: [self], _op: "exp")
+        /// d/dx exp(x) = exp(x) = out.data
         out._backward = {
             self.grad += out.data * out.grad
+        }
+        return out
+    }
+    
+    /// Applies the logarithmic function to a `Value` and returns a new `Value`.
+    ///
+    /// - Returns: A new `Value` representing the result of `log`.
+    func log() -> Value {
+        let out = Value(Foundation.log(self.data), _children: [self], _op: "log")
+        /// d/dx log(x) = 1/x = 1/self.data
+        out._backward = {
+            self.grad += out.grad / self.data
         }
         return out
     }
